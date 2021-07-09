535ab9fc4ddb1db6dfe07de28a6803f2a279b0ce
John Pertuz <Johnpertuz@gmail.com>
2021-07-08 22:36:29 -0500

    Passing the fecthData from callback to promise

---
+++ src/utils/fecthData.js
@@ -0,0 +1,23 @@
+let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+
+
+
+const fecthData = (url_api) => {
+    return new Promise((resolve, reject) => {
+        
+    });
+}
+
+
+ fecthData(API, function(error1, data1){
+     if(error1) return console.error(error1);
+     fecthData(API + data1.results[0].id, function(error2, data2){
+         if(error2) return console.error(error2);
+         fecthData(data2.origin.url, function (error3, data3) {
+             if(error3) return console.error(error3);
+             console.log(`${data1.info.count} Characters`);
+             console.log(`Character: ${data2.name}`);
+             console.log(data3.dimension);
+         });
+     })
+ })
